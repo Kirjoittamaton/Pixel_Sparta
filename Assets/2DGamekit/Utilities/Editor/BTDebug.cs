5c7d2e0f01d442e5d41ebf509476d020c261a6f9
Raccoon623 <113412236+Raccoon623@users.noreply.github.com>
2023-09-19 14:48:17 +0300

    19.9.2023 Enviroments Push 5
    * Created few platforming tiles
    * WATER is now implemented with shader

---
+++ Assets/2DGamekit/Utilities/Editor/BTDebug.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace Gamekit2D
+{
+    public class BTDebug : EditorWindow
+    {
+        private object _currentRoot;
+
+        [MenuItem("Kit Tools/Behaviour Tree Debug")]
+        static void OpenWindow()
+        {
+            BTDebug btdebug = GetWindow<BTDebug>();
+            btdebug.Show();
+        }
+
+        private void OnGUI()
+        {
+            if (!Application.isPlaying)
+            {
+                EditorGUILayout.HelpBox("Only work during play mode.", MessageType.Info);
+            }
+            else
+            {
+                if (_currentRoot == null)
+                    FindRoot();
+                else
+                {
+                    RecursiveTreeParsing(_currentRoot, 0, true);
+                }
+            }
+        }
+
+        private void RecursiveTreeParsing(object currentRoot, int v1, bool v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        void Update()
+        {
+            Repaint();
+        }
+
+        void FindRoot()
+        {
+            if (Selection.activeGameObject == null)
+            {
+                _currentRoot = null;
+                return;
+            }
+
+            IBTDebugable debugable = Selection.activeGameObject.GetComponentInChildren<IBTDebugable>();
+
+            if(debugable != null)
+            {
+            }
+        }
+
+    }
+}
