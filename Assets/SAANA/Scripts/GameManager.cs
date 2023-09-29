7b8a9b91ac324fa0417246ae0f26a28712ea4320
Cerdasune <saanatuulia.tirkkonen@gmail.com>
2023-09-29 18:16:28 +0300

    29.09.2023
    logo and spear -assets included. Menu scene work in progress.

---
+++ Assets/SAANA/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Quit()
+    {
+        //StartCoroutine(Loadlevel(0));
+    }
+}
