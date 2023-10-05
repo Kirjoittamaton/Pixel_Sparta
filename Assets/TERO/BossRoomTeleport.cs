a7502da015aee92c493d3c8071e03b2d9d9f888c
Kirjoittamaton <128027949+Kirjoittamaton@users.noreply.github.com>
2023-10-05 13:50:55 +0300

    5.10.23
    level 1 tekoa

---
+++ Assets/TERO/BossRoomTeleport.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static System.TimeZoneInfo;
+
+public class BossRoomTeleport : MonoBehaviour
+{
+
+    //public AudioClip  audioClip;
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeLevel(int levelNumber)
+    {
+
+        //fadeScreen.SetTrigger("ChangeLevel");
+        StartCoroutine(NewLevel(levelNumber));
+    }
+
+    public IEnumerator NewLevel(int levelNumber)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(levelNumber);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Spartan_Final")
+        {
+            ChangeLevel(5);
+        }
+    }
+}
